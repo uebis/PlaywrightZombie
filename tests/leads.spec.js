592f8c643c2a0951b17a4e385319cf444e17e749
Andrezza Albuquerque <dev.andrezza+git@gmail.com>
2024-09-07 17:22:19 -0300

    lead: clicando no botão e criando um expect

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -3,8 +3,26 @@ const { test, expect } = require('@playwright/test');
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000');
+  await page.getByRole('button', {name: /Aperte o play/}).click();
 
-  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click();
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
 
-  await page.waitForTimeout(10000);
+  await page.locator('input[name=name]').fill('Richas QSMP');
+  await page.getByPlaceholder('Seu email principal').fill('richas@teste.com');
+
+
+  await page.getByTestId('modal') 
+    .getByText('Quero entrar na fila!').click();
+
+  // await page.getByText('seus dados conosco').click();
+  // const content = await page.content();
+  // console.log(content);
+
+  const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+
+  await expect(page.locator('.toast')).toHaveText(message);
+
+  await page.waitForTimeout(5000);
 });
